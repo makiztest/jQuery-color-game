560bdcd79df932be3b02ba7b32403be90ec8584b
Mark Delos Reyes <makizworks@gmail.com>
2017-10-27 09:17:49 -0400

    display picked color and if condition if corect or wrong

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,23 @@ var colors = [
     "rgb(255, 255, 0)",
 ]
 
-$(".square").each(function(arr) {
+var squares = $(".square");
+var pickedColor = colors[3];
+var colorDisplay = $(".colorDisplay");
+var messageDisplay = $(".messageDisplay");
+
+colorDisplay.text(pickedColor);
+
+squares.each(function(arr) {
     $(this).css("backgroundColor", colors[arr]);
+    var clickedColor = $(this).css("backgroundColor")
+    $(this).click(function() {
+        if(clickedColor === pickedColor) {
+            console.log("colorpicked")
+            messageDisplay.text("Correct")
+        } else {
+            $(this).css("backgroundColor", "cadetblue")
+            messageDisplay.text("Try Again")
+        }
+    })
 })
